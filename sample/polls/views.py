4d288874d490e69a95be1b9d30d6cdb7ca6c09ee
Swapnil <swapnildeshmane9@gmail.com>
2022-09-06 14:50:03 +0530

    first commit

---
+++ sample/polls/views.py
@@ -0,0 +1,10 @@
+# polls/views.py
+from rest_framework import viewsets
+
+from . import models
+from . import serializers
+
+
+class QuestionViewSet(viewsets.ModelViewSet):
+    queryset = models.Question.objects.all()
+    serializer_class = serializers.QuestionSerializer
